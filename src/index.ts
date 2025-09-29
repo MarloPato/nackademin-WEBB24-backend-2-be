1dff05d072aef2f1da27e2d7d232c2ce0fc47cc7
Marlo Pato <Marlo_pato123@hotmail.com>
2025-09-29 11:10:57 +0200

    added global HTTPException to index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import { Hono } from "hono";
 import dotenv from "dotenv";
 import courseApp from "./routes/course.js";
 import studentApp from "./routes/student.js";
+import { HTTPException } from "hono/http-exception";
 dotenv.config();
 
-const app = new Hono( {
-  strict: false
+const app = new Hono({
+  strict: false,
 });
 
-const serverStartTime = Date.now()
+const serverStartTime = Date.now();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
@@ -18,18 +19,26 @@ app.route("/courses", courseApp);
 app.route("/students", studentApp);
 
 app.get("/health/", (c) => {
-  const now = Date.now()
-  const uptimeSeconds = Math.floor((now - serverStartTime) / 1000)
+  const now = Date.now();
+  const uptimeSeconds = Math.floor((now - serverStartTime) / 1000);
 
   return c.json({
     status: "ok",
     message: "Service is healthy",
-    uptime:uptimeSeconds,
+    uptime: uptimeSeconds,
     startedAt: new Date(serverStartTime).toISOString(),
     timestamp: new Date().toISOString(),
     version: "1.0.0",
-  })
-})
+  });
+});
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
+  }
+  console.error(err);
+  return c.json({ error: "Internal server error" }, 500);
+});
 
 serve(
   {
